b66f8ea7c9e02d75fcbd56f16ef0aebc8823be24
GBordim <gabrielabordim113@gmail.com>
2024-08-08 09:51:30 -0300

    Update webapp00.py
    mudança na escrita do site

--- webapp00.py
+++ webapp00.py
@@ -3,4 +3,6 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.title("Olá, Massaki!")
+st.title("Olá, Gabi & Gui!")
+st.header("Turma 2EI-S")
+st.subheader("Melhores alunos do SENAI")
